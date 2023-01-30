c5c7f388006313d95df46d3eefd1981ef006c803
Gerhard Seeber <gerhard.seeber@gebro.com>
2023-01-30 02:32:04 +0100

    setup for localization

--- MauiFriendicaMobile/Pages/MainPage.xaml.cs
+++ MauiFriendicaMobile/Pages/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace FriendicaMobile.Pages;
+﻿using FriendicaMobile.Resources;
+using System.Globalization;
+
+namespace FriendicaMobile.Pages;
 
 public partial class MainPage : ContentPage
 {
@@ -11,12 +14,21 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+
+		// TODO: replace the following by a setting in the SettingsPage
+		var switchToCulture = new CultureInfo("es");
+        if (AppResources.Culture.TwoLetterISOLanguageName.Equals("es", StringComparison.InvariantCultureIgnoreCase))
+			switchToCulture = new CultureInfo("en");
+		else if (AppResources.Culture.TwoLetterISOLanguageName.Equals("en", StringComparison.InvariantCultureIgnoreCase))
+			switchToCulture = new CultureInfo("de");
+		LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+
 		count++;
 
 		if (count == 1)
 			CounterBtn.Text = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			CounterBtn.Text = LocalizationResourceManager.Instance["SettingsPageTitle"].ToString();
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
---
+++ MauiFriendicaMobile/Resources/LocalizeExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace FriendicaMobile.Resources;
+
+[ContentProperty(nameof(Name))]
+public class LocalizeExtension : IMarkupExtension<BindingBase>
+{      
+    public string Name { get; set; }
+
+    public BindingBase ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new Binding
+        {
+            Mode = BindingMode.OneWay,
+            Path = $"[{Name}]",
+            Source = LocalizationResourceManager.Instance
+        };
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider) 
+    {
+        return ProvideValue(serviceProvider);
+    }
+}
